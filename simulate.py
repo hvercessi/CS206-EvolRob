e93bde2eed19ccceef1abdbf3e752896ff736bd3
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-20 21:44:39 -0500

    Added object to handle physics

--- simulate.py
+++ simulate.py
@@ -1,2 +1,10 @@
+# CS206 Evolutionary Robotics Assignment 1
+# Halina Vercessi
+# due 01/24/2022
+
 # Use pybullet to simulate the world
+
 import pybullet as p
+
+physicsClient = p.connect(p.GUI)
+p.disconnect()
