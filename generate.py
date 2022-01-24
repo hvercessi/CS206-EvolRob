d76f2884ae79c7527ef90acc8a85d8dc56adb494
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-24 13:18:40 -0500

    Added variables for length, width, height

--- generate.py
+++ generate.py
@@ -1,7 +1,13 @@
-
+# CS206 Evolutionary Robotics 
+# Halina Vercessi
+# Specifies what is in the world
 
 import pyrosim.pyrosim as pyrosim
 
+length = 1
+width = 1
+height = 1
+
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
 pyrosim.End()
