4c55fa49c37ebcdb030d9381b92a2c8bb4d21402
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-24 14:25:49 -0500

    Added box2

--- generate.py
+++ generate.py
@@ -17,4 +17,5 @@ z = 0.5
 
 pyrosim.Start_SDF("box.sdf")
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length,width,height])
 pyrosim.End()
