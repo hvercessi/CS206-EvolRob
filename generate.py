ee0d786b6649d549940ca0daa392894034874456
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-24 13:28:53 -0500

    Changed length, width, height

--- generate.py
+++ generate.py
@@ -4,9 +4,9 @@
 
 import pyrosim.pyrosim as pyrosim
 
-length = 4
-width = 1
-height = 1
+length = 1
+width = 2
+height = 3
 
 pyrosim.Start_SDF("box.sdf")
 pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
