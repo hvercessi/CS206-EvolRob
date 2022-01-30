b1b55537cfdcab099a35de6bfe64fc21355e9387
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-30 12:04:51 -0500

    back to multiple rows with 5 now

--- generate.py
+++ generate.py
@@ -23,22 +23,22 @@ pyrosim.Start_SDF("boxes.sdf")
 #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 #pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length,width,height])
 
-#for h in range(3):
-for j in range(3):
-  for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-    z+=1
-    length = length-(length*0.1)
-    width = width-(width*0.1)
-    height = height-(height*0.1)
-  # Set size back to 1x1x1
-  length = 1
-  width = 1
-  height = 1
-  # Set position to next row over
-  x+=1
-  z = 0.5
-  #x = 1.5
-  #y+=1.0
+for h in range(5):
+  for j in range(5):
+    for i in range(10):
+      pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+      z+=1
+      length = length-(length*0.1)
+      width = width-(width*0.1)
+      height = height-(height*0.1)
+    # Set size back to 1x1x1
+    length = 1
+    width = 1
+    height = 1
+    # Set position to next row over
+    x+=1
+    z = 0.5
+  x = 1.5
+  y+=1.0
 
 pyrosim.End()
