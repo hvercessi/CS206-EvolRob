75b2f83ef047e3ee906d6124e03afda644e23207
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-30 12:36:43 -0500

    changed input file to world.sdf

--- simulate.py
+++ simulate.py
@@ -12,7 +12,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
   p.stepSimulation()
   t.sleep(1/70)
