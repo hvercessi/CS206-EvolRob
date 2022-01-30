3767f0cc7840ff51832d369bb5097d6340839bc1
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-30 13:03:33 -0500

    added robotID

--- simulate.py
+++ simulate.py
@@ -10,7 +10,9 @@ import pybullet_data
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
+
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 for i in range(1000):
