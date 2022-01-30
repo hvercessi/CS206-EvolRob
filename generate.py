87451e53953e1ccfba3091e6410ac6e1b07e140a
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-01-30 11:51:22 -0500

    First try at tower rows

--- generate.py
+++ generate.py
@@ -22,10 +22,20 @@ z2 = 1.5
 pyrosim.Start_SDF("boxes.sdf")
 #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 #pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length,width,height])
-for i in range(10):
-  pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-  z+=1
-  length = length-(length*0.1)
-  width = width-(width*0.1)
-  height = height-(height*0.1)
+
+for j in range(3):
+  for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    z+=1
+    length = length-(length*0.1)
+    width = width-(width*0.1)
+    height = height-(height*0.1)
+  # Set size back to 1x1x1
+  length = 1
+  width = 1
+  height = 1
+  # Set position to next row over
+  x+=0.5
+  z = 0.5
+  
 pyrosim.End()
