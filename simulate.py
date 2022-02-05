c60beb4c8a5fbcac3c13c31b57268f1abde24db1
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-05 11:28:30 -0500

    numpy vector before for loop

--- simulate.py
+++ simulate.py
@@ -16,7 +16,11 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(1000)
+print(backLegSensorValues)
+exit()
 p.loadSDF("world.sdf")
+
 for i in range(1000):
   p.stepSimulation()
   backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
