6556bc53d886010ee6c735a9a7344d941a043d9a
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-05 11:36:05 -0500

    store sensor values

--- simulate.py
+++ simulate.py
@@ -17,15 +17,14 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
-print(backLegSensorValues)
-exit()
 p.loadSDF("world.sdf")
 
 for i in range(1000):
   p.stepSimulation()
-  backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+  backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
   t.sleep(1/70)
   #print(i)
   print(backLegTouch)
 
 p.disconnect()
+print(backLegSensorValues)
