63ee800d7db08a2ed567ba149796b703b7db7770
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-06 14:02:05 -0500

    label / legend added

--- analyze.py
+++ analyze.py
@@ -6,6 +6,8 @@ backLegSensorValues = numpy.load("data/BackLegSensorValues.npy", allow_pickle=Fa
 frontLegSensorValues = numpy.load("data/FrontLegSensorValues.npy", allow_pickle=False, encoding="bytes")
 #print(frontLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues)
-matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg")
+matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg")
+
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
