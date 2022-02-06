7a77a2b4bade271acc6f3ee58fc52af233039251
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-06 13:57:26 -0500

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load("data/backLegSensorValues.npy", allow_pickle=False, encoding="bytes")
+backLegSensorValues = numpy.load("data/BackLegSensorValues.npy", allow_pickle=False, encoding="bytes")
 #print(backLegSensorValues)
 frontLegSensorValues = numpy.load("data/FrontLegSensorValues.npy", allow_pickle=False, encoding="bytes")
 #print(frontLegSensorValues)
