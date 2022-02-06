81fba3107773cfbcab5308560c0522a426d70dfa
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-06 13:56:21 -0500

    load in front leg sensor values file

--- analyze.py
+++ analyze.py
@@ -2,7 +2,12 @@ import numpy
 import matplotlib.pyplot
 
 backLegSensorValues = numpy.load("data/backLegSensorValues.npy", allow_pickle=False, encoding="bytes")
-print(backLegSensorValues)
+#print(backLegSensorValues)
+frontLegSensorValues = numpy.load("data/FrontLegSensorValues.npy", allow_pickle=False, encoding="bytes")
+#print(frontLegSensorValues)
 
 matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.show()
+
+matplotlib.pyplot.plot(frontLegSensorValues)
 matplotlib.pyplot.show()
