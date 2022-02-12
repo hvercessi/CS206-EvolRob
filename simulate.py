012eaceb96bfed47bdf3cf82b1ef5a92c1355b6e
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-12 16:26:06 -0500

    adjusted value of first argument

--- simulate.py
+++ simulate.py
@@ -24,7 +24,7 @@ for i in range(1000):
   p.stepSimulation()
   backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
   frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-  pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
+  pyrosim.Set_Motor_For_Joint(bodyIndex = robotID, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
   
   t.sleep(1/70)
 print(backLegSensorValues)
