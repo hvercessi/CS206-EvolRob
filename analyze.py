abd79ec42510a1a17ef919dc7afd3323b34a918b
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-14 01:13:38 -0500

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -10,6 +10,6 @@ positionValues = numpy.load("data/Position_Values.npy", allow_pickle=False, enco
 #matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=5)
 #matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg", linewidth=2)
 
-matplotlib.pyplot.plot(numpy.linspace(0, 2*(numpy.pi), 1000), positionValues, label="Position Values")
+matplotlib.pyplot.plot(1000, positionValues, label="Position Values")
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
