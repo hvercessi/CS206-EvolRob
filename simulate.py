97ae9f9eced27c0f020dea0aa924897b11098b93
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-14 00:12:32 -0500

    targetPosition = (math.pi)/4.0

--- simulate.py
+++ simulate.py
@@ -25,7 +25,7 @@ for i in range(1000):
   p.stepSimulation()
   backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
   frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-  pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = -(math.pi)/4.0, maxForce = 500)
+  pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = (math.pi)/4.0, maxForce = 500)
   
   t.sleep(1/70)
 print(backLegSensorValues)
