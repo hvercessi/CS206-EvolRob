4823b413b88d35b1481e831c3db167c94f122337
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-14 01:11:20 -0500

    plot position values

--- analyze.py
+++ analyze.py
@@ -5,9 +5,11 @@ backLegSensorValues = numpy.load("data/BackLegSensorValues.npy", allow_pickle=Fa
 #print(backLegSensorValues)
 frontLegSensorValues = numpy.load("data/FrontLegSensorValues.npy", allow_pickle=False, encoding="bytes")
 #print(frontLegSensorValues)
+positionValues = numpy.load("data/Position_Values.npy", allow_pickle=False, encoding="bytes")
 
-matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=5)
-matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg", linewidth=2)
+#matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=5)
+#matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg", linewidth=2)
 
+matplotlib.pyplot.plot(numpy.linspace(0, 2*(numpy.pi), 1000), positionValues, label="Position Values")
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
