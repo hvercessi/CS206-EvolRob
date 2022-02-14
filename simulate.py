383fa26fba81a2bf1ef5e1fedeff4f98c0f0083b
hvercessi <59889728+hvercessi@users.noreply.github.com>
2022-02-14 01:06:15 -0500

    save position values to file

--- simulate.py
+++ simulate.py
@@ -22,6 +22,9 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 
+position_seq_vect = numpy.linspace(0, 2*(numpy.pi), 1000)
+translated_pos_vals = numpy.sin(position_seq_vect)
+
 for i in range(1000):
   p.stepSimulation()
   backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
@@ -30,15 +33,18 @@ for i in range(1000):
   targetPos1 = targetPos1*((math.pi)/2.0 - (-(math.pi)/2.0)) + (-(math.pi)/2.0)
   targetPos2 = rand.random()
   targetPos2 = targetPos2*((math.pi)/2.0 - (-(math.pi)/2.0)) + (-(math.pi)/2.0)
+  
   pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetPos1, maxForce = 500)
   pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetPos2, maxForce = 500)
   
   t.sleep(1/70)
 print(backLegSensorValues)
 print(frontLegSensorValues)
-with open('data/BackLegSensorValues.npy', 'wb') as f:
-  numpy.save(f, backLegSensorValues)
-with open('data/FrontLegSensorValues.npy', 'wb') as f:
-  numpy.save(f, frontLegSensorValues)
+with open('data/BackLegSensorValues.npy', 'wb') as f1:
+  numpy.save(f1, backLegSensorValues)
+with open('data/FrontLegSensorValues.npy', 'wb') as f2:
+  numpy.save(f2, frontLegSensorValues)
+with open('data/Position_Values.npy', 'wb') as f3:
+  numpy.save(f3,  translated_pos_vals)
 p.disconnect()
 
