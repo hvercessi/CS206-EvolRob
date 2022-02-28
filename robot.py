b0fb62f05cfff11bab42bdb9d88ac7b1a882cfba
hvercessi <hvercess@uvm.edu>
2022-02-28 17:09:01 -0500

    clean-up

--- robot.py
+++ robot.py
@@ -33,8 +33,8 @@ class ROBOT:
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
                 self.motors[jointName].Set_Value(self, desiredAngle)
-                print(neuronName + ", " + jointName + ", ", end="")
-                print(desiredAngle)
+                #print(neuronName + ", " + jointName + ", ", end="")
+                #print(desiredAngle)
         
             
             
