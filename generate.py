04e4001fb34f88cd2a8ef026c7f5d28e62f6b077
hvercessi <hvercess@uvm.edu>
2022-03-13 20:36:56 -0400

    more synapses

--- generate.py
+++ generate.py
@@ -53,11 +53,10 @@ def Generate_Brain():
   # pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
   # pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 3 , weight = 1.0 )
   
-  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1.0 )
-  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
-  #pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -10.0 )
-  pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -4.0 )
-
+  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.0 )
+  pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -1.0 )
+  pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0 )
+  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0 )
 
   
   
