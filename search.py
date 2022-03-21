0ff0e162b974b507ed8b4be44b103550b15f08f8
hvercessi <hvercess@uvm.edu>
2022-03-21 11:42:42 -0400

    use parallel hill climber in search.py

--- search.py
+++ search.py
@@ -2,12 +2,17 @@
 import os
 import time as t
 import subprocess
-from hillclimber import HILLCLIMBER
+#from hillclimber import HILLCLIMBER
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import os
 
 subprocess.call("python generate.py")
 subprocess.call("python simulate.py GUI")
 
-hc = HILLCLIMBER()
-hc.Evolve()
-hc.Show_Best()
+# hc = HILLCLIMBER()
+# hc.Evolve()
+# hc.Show_Best()
+
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()
+phc.Show_Best()
