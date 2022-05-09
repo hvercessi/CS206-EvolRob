2b87b0bcf257a527538b288c900b46e8978fc9c0
hvercessi <hvercess@uvm.edu>
2022-05-09 11:31:30 -0400

    summ

--- boxplot.py
+++ boxplot.py
@@ -43,10 +43,10 @@ with open("SolutionFitnessB.txt", 'r') as f:
         line = f.readline()
 
 generationsB = np.array(generationsB)
-fitValuesB = np.array(fitValuesB)
+fitValuesB = np.around(np.array(fitValuesB), 3)
 
 generationsA = np.array(generationsA)
-fitValuesA = np.array(fitValuesA)
+fitValuesA = np.around(np.array(fitValuesA), 3)
 
 # mA, bA = np.polyfit(generationsA, fitValuesA, 1)
 
@@ -65,12 +65,34 @@ fig, ax = plt.subplots()
 # third_legend = ax.legend([(scatterA, scatterB)])
 
 # ax.legend([scatterA, scatterB, lineA, lineB], ["A", "B", "Fit Line A", "Fit Line B"])
+
+Aq1 = "   Q1 Quantile: "+str(np.quantile(fitValuesA, 0.25))
+Aq2 = "   Q2 Quantile: "+str(np.quantile(fitValuesA, 0.5))
+Aq3 = "   Q3 Quantile: "+str(np.quantile(fitValuesA, 0.75))
+Aavg = "   Average Fitness: " + str(np.around(np.mean(fitValuesA), 3))
+
+Bq1 = "   Q1 Quantile: "+str(np.quantile(fitValuesB, 0.25))
+Bq2 = "   Q2 Quantile: "+str(np.quantile(fitValuesB, 0.5))
+Bq3 = "   Q3 Quantile: "+str(np.quantile(fitValuesB, 0.75))
+Bavg = "   Average Fitness: " + str(np.around(np.mean(fitValuesB), 3))
+
 data = [fitValuesA, fitValuesB]
-ax.boxplot(data)
+# ax.boxplot(data)
 
-ax.set_xticklabels(["A", "B"])
-plt.xlabel("Test Group")
-plt.ylabel("Fitness")
-plt.title("Fitness after 15 Generations")
-plt.show()
+print("\n   Test A Summary:\n" +Aq1 +"\n")
+print(Aq2 +"\n")
+print(Aq3 +"\n")
+print(Aavg +"\n")
 
+print("   ____________________\n\n   Test B Summary:\n" +Bq1 +"\n")
+print(Bq2 +"\n")
+print(Bq3 +"\n")
+print(Bavg +"\n")
+# ax.set_xticklabels(["A", "B"])
+# plt.xlabel("Test Group")
+# plt.ylabel("Fitness")
+# plt.title("Fitness after 15 Generations")
+# plt.show()
+
+#plt.text(2, 2, q1, fontsize = 10, color = 'g')
+#plt.show()
